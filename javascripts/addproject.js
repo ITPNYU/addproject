807767ee8d2085460f03f6808075be9a40e1c435
Steve Klise <sk@skli.se>
2013-08-10 16:52:01 -0400

    Quick and dirty

---
+++ javascripts/addproject.js
@@ -0,0 +1,22 @@
+$(function(){
+  $('.chosen-select').chosen({no_results_text: "Click to choose options"});
+
+  $('#submit-form').click(function () {
+    var validForm = true;
+    var errors = [];
+
+    $('.input-item.required').each(function () {
+      var i = $(this).find('input,select,textarea').first();
+      console.log(i.val())
+      if (i.val() === null || i.val() === "") {
+        validForm = false;
+        errors.push("Please fill in the " + i.closest('.input-item').find('label').text() + " field");
+      }
+    });
+
+    if (!validForm) {
+      $('#form-status').html(errors.join("<br>"));
+      return false;
+    }
+  });
+});
